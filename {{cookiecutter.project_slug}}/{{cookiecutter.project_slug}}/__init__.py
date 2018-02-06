cf9555f38a84c7ff32094053f8e55da20c776e55
Gaetan Semet <gaetan@xeberon.net>
2018-02-06 18:23:01 +0100

    enhancements

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -2,14 +2,19 @@
 
 import pbr.version
 
+import {{ cookiecutter.project_slug }}._{{ cookiecutter.project_slug }} import MyPublicClass
+
 
 def version():
     return pbr.version.VersionInfo('{{ cookiecutter.project_slug }}').release_string()
 
-
+## Uncomment the following line to declare a __version__ in your root module. Beware the evaluation
+## of the version may impact the load time of your module
+#
 # __version__ = version()
 #
 #
-# __all__ = [
+__all__ = [
 #     '__version__',
-# ]
+    'MyPublicClass',
+]
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/_{{cookiecutter.project_slug}}.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class MyPublicClass(object):
+    pass
